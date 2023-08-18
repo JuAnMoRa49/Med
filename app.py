64fd467e7a95af07453f3c8a0546a8af4e23a8cb
Danny <121039646@upq.edu.mx>
2023-08-17 21:41:05 -0600

    avance del examen entregado 17/08/23

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ app.secret_key = 'mysecretkey'
 
 mysql = MySQL(app)
 
+bcrypt = Bcrypt(app)
+
 def get_cursor():
     return mysql.connection.cursor()
 
@@ -31,6 +33,16 @@ def login_required(f):
         return f(*args, **kwargs)
     return decorated_function
 
+def obtener_rol(rfc):
+    cursor = get_cursor()
+    query = 'SELECT rol FROM Medicos WHERE RFC = %s'
+    cursor.execute(query, (rfc,))
+    resultado = cursor.fetchone()
+    
+    if resultado:
+        return resultado[0]
+    else:
+        return None
 
 
 
@@ -41,13 +53,18 @@ def login():
     vpassword = request.form['password']
     CS= mysql.connection.cursor()
     
-    consulta= 'select RFC from Medicos where RFC = %s and password = %s'
-    CS.execute(consulta, (vrfc, vpassword))
-    resultado = CS.fetchone()
+    # consulta= 'select RFC from Medicos where RFC = %s and password = %s'
+    # CS.execute(consulta, (vrfc, vpassword))
+    # resultado = CS.fetchone()
+    cursor = get_cursor()
+    query = 'SELECT RFC, password FROM Medicos WHERE RFC = %s'
+    cursor.execute(query, (vrfc,))
+    resultado = cursor.fetchone()
     
     if resultado is not None:
         session['rfc_user'] = vrfc
-        return redirect(url_for('admi_medi'))
+        session['rol'] = obtener_rol(vrfc)
+        return redirect(url_for('cons_cita'))
     else:
         flash('RFC o contraseña incorrectos')
         return redirect(url_for('index'))
@@ -58,6 +75,11 @@ def login():
 @app.route('/admi_medi', methods=['POST', 'GET'])
 @login_required
 def admi_medi():
+    
+    if session.get('rol') == 'Medico':
+        flash('Acceso denegado: No tienes permiso para acceder a esta página.', 'error')
+        return redirect(url_for('cons_cita'))
+    
     if request.method == 'POST':
         rfc = request.form.get('rfc')
         nombre = request.form.get('nombre')
@@ -75,10 +97,11 @@ def admi_medi():
         elif rol == 'medico':
             rol = 'Médico'
 
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')  # Encriptar la contraseña
         cursor = get_cursor()
         query = 'INSERT INTO Medicos (RFC, nombreCompleto, cedulaProfesional, correo, password, rol) ' \
                 'VALUES (%s, %s, %s, %s, %s, %s)'
-        cursor.execute(query, (rfc, nombre, cedula, correo, password, rol))
+        cursor.execute(query, (rfc, nombre, cedula, correo, hashed_password, rol))
         mysql.connection.commit()
         flash('Registro exitosamente!') 
         return redirect(url_for('admi_medi'))
@@ -90,6 +113,11 @@ def admi_medi():
 @app.route('/busc_medi', methods=['GET', 'POST'])
 @login_required
 def buscar():
+    
+    if session.get('rol') == 'Medico':
+        flash('Acceso denegado: No tienes permiso para acceder a esta página.', 'error')
+        return redirect(url_for('cons_cita'))
+    
     cursor = get_cursor()
     user = None
 
@@ -131,10 +159,11 @@ def actu_medi():
     password = request.form.get('password')
     rol = request.form.get('rol')
 
+    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
     query = '''UPDATE Medicos 
                SET nombreCompleto = %s, cedulaProfesional = %s, correo = %s, password = %s, rol = %s 
                WHERE RFC = %s'''
-    cursor.execute(query, (nombre, cedula, correo, password, rol, rfc))
+    cursor.execute(query, (nombre, cedula, correo, hashed_password, rol, rfc))
     mysql.connection.commit()
 
     flash('Registro actualizado correctamente', 'success')
@@ -142,7 +171,90 @@ def actu_medi():
     return redirect(url_for('buscar'))
 
 
+@app.route('/eliminar_medico/<rfc>', methods=['POST'])
+@login_required
+def eliminar_medico(rfc):
+    cursor = get_cursor()
 
+    # Eliminar el médico de la base de datos
+    query = 'DELETE FROM Medicos WHERE RFC = %s'
+    cursor.execute(query, (rfc,))
+    mysql.connection.commit()
+
+    flash('Registro eliminado correctamente', 'success')
+    return redirect(url_for('admi_medi'))
+
+
+
+
+@app.route('/cons_paci')
+@login_required
+def cons_paci():
+    cursor = get_cursor()
+
+    # Obtener el idMedico del médico que ha iniciado sesión
+    rfc_medico = session['rfc_user']
+    cursor.execute('SELECT idMedico FROM Medicos WHERE RFC = %s', (rfc_medico,))
+    id_medico = cursor.fetchone()[0]
+
+    # Consultar los pacientes registrados por el médico
+    query = '''SELECT Pacientes.idPaciente, Medicos.nombreCompleto AS nombreMedico, Pacientes.nombreCompleto AS nombrePaciente,
+                      Pacientes.fechaNacimiento, Pacientes.enfermedadesCronicas, Pacientes.alergias, Pacientes.antecedentesFamiliares
+               FROM Pacientes
+               JOIN Medicos ON Pacientes.idMedico = Medicos.idMedico
+               WHERE Medicos.idMedico = %s'''
+    cursor.execute(query, (id_medico,))
+    pacientes = cursor.fetchall()
+
+    return render_template('consultarPacientes.html', pacientes=pacientes
+)
+
+
+@app.route('/editar_paciente/<int:idPaciente>', methods=['GET', 'POST'])
+@login_required
+def editar_paciente(idPaciente):
+    # Aquí deberías implementar la lógica para editar el paciente con el ID proporcionado
+    # y luego renderizar la plantilla de edición de paciente con los datos prellenados
+    
+    return render_template('editar_paciente.html', idPaciente = idPaciente)
+
+
+
+@app.route('/actualizar_paciente/<int:idPaciente>', methods=['POST'])
+@login_required
+def actualizar_paciente(idPaciente):
+    cursor = get_cursor()
+
+    nombrePaciente = request.form.get('nombrePaciente')
+    fechanacimiento = request.form.get('fechanacimiento')
+    enfermedades = request.form.get('enfermedades')
+    alergias = request.form.get('alergias')
+    antecedentes = request.form.get('antecedentes')
+
+    query = '''UPDATE Pacientes 
+               SET nombrePaciente = %s, fechanacimiento = %s, enfermedadesCronicas = %s, alergias = %s, antecedentesFamiliares = %s
+               WHERE idPaciente = %s'''
+    cursor.execute(query, (nombrePaciente, fechanacimiento, enfermedades, alergias, antecedentes, idPaciente))
+    mysql.connection.commit()
+
+    flash('Paciente actualizado correctamente', 'success')
+
+    return redirect(url_for('cons_paci'))
+
+
+
+@app.route('/eliminar_paciente/<int:idPaciente>', methods=['POST'])
+@login_required
+def eliminar_paciente(idPaciente):
+    cursor = get_cursor()
+
+    # Eliminar al paciente de la base de datos
+    query = 'DELETE FROM Pacientes WHERE idPaciente = %s'
+    cursor.execute(query, (idPaciente,))
+    mysql.connection.commit()
+
+    flash('Registro de paciente eliminado correctamente', 'success')
+    return redirect(url_for('cons_paci'))
 
 
 @app.route('/cons_cita', methods=['GET', 'POST'])
@@ -150,30 +262,42 @@ def actu_medi():
 def cons_cita():
     cursor = get_cursor()
     citas = None
+    busqueda_realizada = False  # Variable que indica si se ha hecho una búsqueda
 
     if request.method == 'POST':
-        nombreMedico = request.form.get('nombreMedico')
-        fecha = request.form.get('fecha')
+        nombrePaciente = request.form.get('nombre')
+        print(f"Buscando citas para el paciente: {nombrePaciente}")  # Debugging
+        busqueda_realizada = True  # Cambia el valor a True cuando se realiza una búsqueda
 
-        if nombreMedico:
-            query = '''SELECT Medicos.nombreCompleto AS nombreMedico, Pacientes.nombreCompleto AS nombrePaciente, Citas.*
-                       FROM Citas
-                       JOIN Pacientes ON Citas.idPaciente = Pacientes.idPaciente
-                       JOIN Medicos ON Pacientes.idMedico = Medicos.idMedico
-                       WHERE Medicos.nombreCompleto LIKE %s'''
-            cursor.execute(query, ('%' + nombreMedico + '%',))
-            citas = cursor.fetchall()  # Ejecutar y obtener los resultados
-        elif fecha:
-            query = '''SELECT Medicos.nombreCompleto AS nombreMedico, Pacientes.nombreCompleto AS nombrePaciente, Citas.*
-                       FROM Citas
-                       JOIN Pacientes ON Citas.idPaciente = Pacientes.idPaciente
-                       JOIN Medicos ON Pacientes.idMedico = Medicos.idMedico
-                       WHERE Citas.fecha = %s'''
-            cursor.execute(query, (fecha,))
-            citas = cursor.fetchall()  # Ejecutar y obtener los resultados
-        
+        if nombrePaciente:
+            query = '''
+                SELECT 
+                    Medicos.nombreCompleto AS nombreMedico, 
+                    Pacientes.nombreCompleto AS nombrePaciente, 
+                    Citas.sintomas, Citas.diagnostico, Citas.tratamiento,
+                    Citas.fecha  -- Aquí se agrega el campo fecha
+                FROM Citas
+                JOIN Pacientes ON Citas.idPaciente = Pacientes.idPaciente
+                JOIN Medicos ON Pacientes.idMedico = Medicos.idMedico
+                WHERE Pacientes.nombreCompleto LIKE %s
+            '''
 
-    return render_template('cons_cita.html', citas=citas)
+            print(f"Query: {query}")  # Debugging
+            cursor.execute(query, ('%' + nombrePaciente + '%',))
+            citas = cursor.fetchall()
+
+            # Debugging
+            if citas:
+                print(type(citas[0]))
+
+            if not citas:
+                print("No se encontraron citas para el paciente.")
+            else:
+                print(f"Citas encontradas: {len(citas)}")
+                for cita in citas:
+                    print(cita)
+
+    return render_template('cons_cita.html', citas=citas, busqueda_realizada=busqueda_realizada)
 
 
 
@@ -215,6 +339,7 @@ def regi_expl():
 def logout():
     session.pop('rfc_user', None)
     return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
